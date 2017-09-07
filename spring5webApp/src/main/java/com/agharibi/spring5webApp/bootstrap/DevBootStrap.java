919033ab23c5d38cbaa6b0b1a52ac08d6267e2eb
Armen G <gharibi.armen@yahoo.com>
2017-09-07 08:21:35 -0700

    added publisher entity added publisher JPA data repository added publisher to bootstrap data

--- spring5webApp/src/main/java/com/agharibi/spring5webApp/bootstrap/DevBootStrap.java
+++ spring5webApp/src/main/java/com/agharibi/spring5webApp/bootstrap/DevBootStrap.java
@@ -2,8 +2,10 @@ package com.agharibi.spring5webApp.bootstrap;
 
 import com.agharibi.spring5webApp.model.Author;
 import com.agharibi.spring5webApp.model.Book;
+import com.agharibi.spring5webApp.model.Publisher;
 import com.agharibi.spring5webApp.repositories.AuthorRepository;
 import com.agharibi.spring5webApp.repositories.BookRepository;
+import com.agharibi.spring5webApp.repositories.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -19,6 +21,8 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent>{
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private PublisherRepository publisherRepository;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
@@ -28,17 +32,22 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent>{
 
     private void initData() {
 
+        Publisher publisher1 = new Publisher("Harper Collions", "1200 Main Street, Washington");
         Author eric = new Author("Eric", "Evans");
-        Book book1 = new Book("Domain Drive Desing", "1234", "Harper Collions");
+        Book book1 = new Book("Domain Drive Desing", "1234", publisher1);
         eric.getBooks().add(book1);
 
+        publisherRepository.save(publisher1);
         authorRepository.save(eric);
         bookRepository.save(book1);
 
+
+        Publisher publisher2 = new Publisher("Penguins", "4600 First Street, San Diego, CA");
         Author rod = new Author("Rod", "Johnson");
-        Book book2 = new Book("Spring Framework", "5600", "Penguins");
+        Book book2 = new Book("Spring Framework", "5600", publisher2);
         rod.getBooks().add(book2);
 
+        publisherRepository.save(publisher2);
         authorRepository.save(rod);
         bookRepository.save(book2);
     }
---
+++ spring5webApp/src/main/java/com/agharibi/spring5webApp/repositories/PublisherRepository.java
@@ -0,0 +1,8 @@
+package com.agharibi.spring5webApp.repositories;
+
+import com.agharibi.spring5webApp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
